def053fa7c8357e4bc7a5a233be06b787e928f6c
Keith Napolitano <napolitanokrke@gmail.com>
2022-12-29 14:10:18 +0800

    new tables

--- database/migrations/2022_11_14_160109_create_vans_table.php
+++ database/migrations/2022_11_14_160109_create_vans_table.php
@@ -17,7 +17,12 @@ return new class extends Migration
             Schema::create('vans', function (Blueprint $table) {
                 $table->id();
                 $table->string('plate_num');
+                $table->unsignedBigInteger('user_id');
+                $table->unsignedBigInteger('route_id');
                 $table->timestamps();
+
+                $table->foreign('user_id')->references('id')->on('users');
+                $table->foreign('route_id')->references('id')->on('routes');
             });
         }
     }
