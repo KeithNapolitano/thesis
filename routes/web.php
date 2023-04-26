e2b0ee02f255a6111177714e8412b665ca95635b
Keith Napolitano <napolitanokrke@gmail.com>
2023-04-26 23:27:43 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\FallbackController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
