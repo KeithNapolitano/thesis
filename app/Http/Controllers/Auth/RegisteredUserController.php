906c5f886d03564ae9f628816d64d6801d5cfd3e
Keith Napolitano <napolitanokrke@gmail.com>
2023-04-27 11:04:24 +0800

    register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -52,6 +52,16 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(RouteServiceProvider::HOME);
+        // return redirect(RouteServiceProvider::HOME);
+
+        if(Auth::user()->userlvl == 3){     //admin
+            return redirect('/trip/create')->with('status', 'Welcome to Admin Dashboard');
+        }
+        else if(Auth::user()->userlvl == 2){   //ope
+            return redirect('/operator/schedule')->with('status', 'Welcome to Operator Dashboard');
+        }
+        else{   //user
+            return redirect('/commuter')->with('status', 'Welcome to your Dashboard');
+        }
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,6 +50,12 @@
                     {{ __('Log in') }}
                 </x-primary-button>
             </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                    Sign up
+                </a>
+            </div>
         </form>
     </x-auth-card>
 </x-guest-layout>
