ae4e6167b6002fe5064a7cbf6c9af6062aed6581
Nepzd <112059606+Nepzd@users.noreply.github.com>
2023-04-27 09:23:01 +0800

    new changes

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -129,7 +129,7 @@ class TripController extends Controller
 
         return redirect('/trip/create')->with('message', 'Trip has been updated.');
     }
-    public function QRupdate(Request $request, $id)
+    public function QRupdate(Request $request)
     {
         $output = $request->input('output');
 
